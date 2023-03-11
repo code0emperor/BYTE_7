c8e64a53779d0ff126805391775c5b97b1c2f21a
code0emperor <rr912072@student.nitw.ac.in>
2023-03-11 22:28:20 +0530

    Filter update

--- server/controller/client.js
+++ server/controller/client.js
@@ -22,7 +22,25 @@ exports.allGuides = (req, res) => {
   }
 };
 
-exports.filter = (req, res) => {
+const userGenerate = async (guides) => {
+  let users = [];
+  guides.map(async (guide) => {
+    // console.log(guide);
+    console.log(guide[0].language);
+    await User.findById(guide[0].id).then((user, err) => {
+      console.log(err);
+      console.log(user);
+      if (err) {
+        return res.status(400).json({ message: err.message, success: false });
+      }
+      // console.log(guide);
+      users.push(user);
+    });
+  });
+  return users;
+};
+
+exports.filter = async (req, res) => {
   try {
     const location = req.body.location || [];
     const availability = req.body.availability || true;
@@ -39,23 +57,39 @@ exports.filter = (req, res) => {
     // filters.availability = availability;
     if (gender.length >= 1) filters.gender = gender;
 
-    Guide.find(filters, (err, guide) => {
+    await Guide.find(filters).then((guide, err) => {
       if (err) {
-        return res.status(400).json({ message: err.message, success: false });
+        console.log("Hello");
+        return res.status(400).json({ message: err, success: false });
       }
       guides.push(guide);
+      // console.log(guide);
       check = true;
     });
+
     console.log(guides);
+
+    // const users = await userGenerate(guides);
+
     let users = [];
-    guides.map((guide) => {
-      User.findById(guide.id, (err, user) => {
+    guides.map(async (guide) => {
+      // console.log(guide);
+      // console.log(guide[0].language);
+      await User.findById(guide[0].id).then((user, err) => {
+        console.log(err);
+        console.log(user);
         if (err) {
           return res.status(400).json({ message: err.message, success: false });
         }
+        // console.log(guide);
         users.push(user);
       });
     });
+
+    // while (users.length !== guides.length)
+    // console.log(users.length + " " + guides.length);
+
+    console.log(users + "Hello");
     for (var i = 0; i < guides.length; i++) {
       res.status(200).json({
         user: users[i],
